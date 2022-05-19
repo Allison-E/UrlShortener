8f3d27c00494628e0e867a6b990e6e8362a9947f
Emmanuel Allison <emmallison13@gmail.com>
2022-05-19 15:32:28 +0100

    Added NotFoundException

---
+++ Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Application.Exceptions;
+
+/// <summary>
+/// Represents errors that occur when a requested resource is not found.
+/// </summary>
+public class NotFoundException: Exception
+{
+    public NotFoundException(): base("Resource not found.")
+    {
+    }
+
+    public NotFoundException(string message): base (message)
+    {
+    }
+}
